0127da86db361234b736f2ada46219c062c85417
Rohan <rohanpschauhan@gmail.com>
2024-07-09 20:21:01 +0100

    Added function to show input of numbers on click

---
+++ scripts/calculator.js
@@ -0,0 +1,7 @@
+function input(x) {
+    if (document.getElementById('result').value == 0) { // This checks the 0 and gets rid of it when entering a number fo rthe first time
+        document.getElementById('result').value = x;
+    } else {
+        document.getElementById('result').value += x;
+    }
+}
